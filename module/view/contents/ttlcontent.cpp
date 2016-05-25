76e75ddb9726bf4cbc82a58e124c48701b36bb75
杨万青 <yangwanqing@linuxdeepin.com>
2016-05-25 15:05:13 +0800

    View: update the style of back-to-main button
    Change-Id: Ia0aad36388ecf9a4b7efd643012a0776485eb205

--- module/view/contents/ttlcontent.cpp
+++ module/view/contents/ttlcontent.cpp
@@ -4,16 +4,16 @@
 TTLContent::TTLContent(QWidget *parent) : QWidget(parent)
 {
     QHBoxLayout *hb = new QHBoxLayout(this);
-    hb->setContentsMargins(0, 0, 0, 0);
+    hb->setContentsMargins(5, 0, 0, 0);
     hb->setSpacing(0);
 
     ImageButton *btn = new ImageButton();
-    btn->setNormalPic(":/images/resources/images/album_normal.png");
-    btn->setHoverPic(":/images/resources/images/album_hover.png");
-    btn->setPressPic(":/images/resources/images/album_active.png");
+    btn->setNormalPic(":/images/resources/images/folder_normal.png");
+    btn->setHoverPic(":/images/resources/images/folder_hover.png");
+    btn->setPressPic(":/images/resources/images/folder_press.png");
     hb->addWidget(btn);
     connect(btn, &ImageButton::clicked, this, [=] {
         emit backToMain();
     });
-    btn->setToolTip(tr("Back"));
+    btn->setToolTip(tr("Image management"));
 }
