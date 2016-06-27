7863587539bd8561ecf38d4291ea13a6a45bd322
pangpangpang3 <penghui@linuxdeepin.com>
2016-06-27 20:25:36 +0800

    Adjust navigation window's ui
    Change-Id: Id7b70aa77c69a6ef394a173b5fddff275d080832

--- module/view/navigationwidget.cpp
+++ module/view/navigationwidget.cpp
@@ -3,6 +3,7 @@
 #include <QPainter>
 #include <dwindowclosebutton.h>
 #include <QMouseEvent>
+#include <QGraphicsDropShadowEffect>
 #include <QtDebug>
 
 namespace {
@@ -14,15 +15,21 @@ const QString SETTINGS_ALWAYSHIDDEN_KEY = "NavigationAlwaysHidden";
 
 using namespace Dtk::Widget;
 
+const int IMAGE_MARGIN = 3;
 NavigationWidget::NavigationWidget(QWidget *parent)
     : QWidget(parent)
 {
-    resize(200, 150);
+    resize(150, 112);
 
     DWindowCloseButton *b = new DWindowCloseButton(this);
+    b->move(QPoint(this->x() + this->width() - 28,
+                   rect().topRight().y() - 1));
     connect(b, &DWindowCloseButton::clicked, [this](){
         setAlwaysHidden(true);
     });
+
+    m_mainRect = QRect(rect().x() + IMAGE_MARGIN, rect().y() + IMAGE_MARGIN,
+                       rect().width() - IMAGE_MARGIN*2, rect().height() - IMAGE_MARGIN*2);
 }
 
 void NavigationWidget::setAlwaysHidden(bool value)
@@ -43,10 +50,19 @@ bool NavigationWidget::isAlwaysHidden() const
 
 void NavigationWidget::setImage(const QImage &img)
 {
-    m_img = img.scaled(rect().size(), Qt::KeepAspectRatio);
+    QRect tmpImageRect = QRect(m_mainRect.x()+1, m_mainRect.y()+1,
+                               m_mainRect.width() - IMAGE_MARGIN*2, m_mainRect.height() - IMAGE_MARGIN*2);
+    m_img = img.scaled(tmpImageRect.size(), Qt::KeepAspectRatio);
+
     m_pix = QPixmap::fromImage(m_img);
-    m_imageScale = qreal(m_img.width())/qreal(img.width());
-    m_r = QRect(0, 0, m_img.width(), m_img.height());
+
+    if (img.width() > img.height()) {
+        m_imageScale = qreal(m_img.width())/qreal(img.width());
+    } else {
+        m_imageScale = qreal(m_img.height())/qreal(img.height());
+    }
+
+    m_r = QRect(IMAGE_MARGIN, IMAGE_MARGIN, m_img.width(), m_img.height());
     update();
 }
 
@@ -73,16 +89,24 @@ void NavigationWidget::mouseMoveEvent(QMouseEvent *e)
 
 void NavigationWidget::tryMoveRect(const QPoint &p)
 {
-    const int x0 = (width()-m_img.width())/2;
-    const int y0 = (height()-m_img.height())/2;
+    const int x0 = (m_mainRect.width()-m_img.width())/2;
+    const int y0 = (m_mainRect.height()-m_img.height())/2;
     const QRect r(x0, y0, m_img.width(), m_img.height());
     if (!r.contains(p))
         return;
-    const int x = qMax(0, qMin(p.x() - x0 - m_r.width()/2, m_img.width()-m_r.width()));
-    const int y = qMax(0, qMin(p.y() - y0 - m_r.height()/2, m_img.height()-m_r.height()));
+    const int x = qMax(IMAGE_MARGIN, qMin(p.x() - x0 - m_r.width()/2, m_img.width()-m_r.width()));
+    const int y = qMax(IMAGE_MARGIN, qMin(p.y() - y0 - m_r.height()/2, m_img.height()-m_r.height()));
 
     //qDebug("request move: %d %d %.1f %.1f", x, y, (qreal)x/m_imageScale, (qreal)y/m_imageScale);
     Q_EMIT requestMove((qreal)x/m_imageScale, (qreal)y/m_imageScale);
+}
+
+void NavigationWidget::addDropShadow() {
+    QGraphicsDropShadowEffect *dropShadow = new QGraphicsDropShadowEffect;
+    dropShadow->setBlurRadius(8);
+    dropShadow->setColor(QColor(0, 0, 0, 128));
+    dropShadow->setOffset(0, 4);
+    this->setGraphicsEffect(dropShadow);
 }
 
 void NavigationWidget::paintEvent(QPaintEvent *)
@@ -94,13 +118,18 @@ void NavigationWidget::paintEvent(QPaintEvent *)
     }
     QImage img(m_img);
     QPainter p(&img);
-    p.setRenderHint(QPainter::Antialiasing);
+    p.setRenderHints(QPainter::Antialiasing|QPainter::HighQualityAntialiasing);
     p.setRenderHint(QPainter::SmoothPixmapTransform);
     p.setClipRegion(QRegion(0, 0, img.width(), img.height()) - m_r);
-    p.fillRect(QRect(0, 0, m_img.width(), m_img.height()), QColor(0, 0, 0, 100));
+    p.fillRect(QRect(0, 0, m_img.width(), m_img.height()), QColor(0, 0, 0, 150));
     p.end();
     p.begin(this);
-    p.fillRect(rect(), QColor(0, 0, 0, 100));
-    p.drawImage((width()-m_img.width())/2, (height()-m_img.height())/2, img);
+
+    p.setPen(QColor(255, 255, 255, 76));
+    p.setBrush(QBrush(QColor(0, 0, 0, 100)));
+    p.drawRoundedRect(m_mainRect, 3, 3);
+
+    p.drawImage(QRect((m_mainRect.width()-m_img.width())/2 + IMAGE_MARGIN,
+                (m_mainRect.height()-m_img.height())/2 + IMAGE_MARGIN, img.width(), img.height()), img);
     p.end();
 }
