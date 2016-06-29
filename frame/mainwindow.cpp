5bbd442fe8c0640f0a9a41fa28dc8691b3d42942
杨万青 <yangwanqing@linuxdeepin.com>
2016-06-29 15:55:26 +0000

    Record window's size after resize
    Change-Id: Iae998594a5400aa2c964c69a2bb9e673cb6c8b93

--- frame/mainwindow.cpp
+++ frame/mainwindow.cpp
@@ -1,22 +1,47 @@
 #include "mainwindow.h"
 #include "mainwidget.h"
+#include "controller/configsetter.h"
 #include <QDesktopWidget>
 
+namespace {
 
 const int MAINWIDGET_MINIMUN_WIDTH = 450;
 const int MAINWIDGET_MINIMUN_HEIGHT = 450;
+const QString SETTINGS_GROUP = "MAINWINDOW";
+const QString SETTINGS_WINSIZE_W_KEY = "WindowWidth";
+const QString SETTINGS_WINSIZE_H_KEY = "WindowHeight";
+
+}  //namespace
 
 MainWindow::MainWindow(bool manager, QWidget *parent):
-    DWindowFrame(parent)
+    DWindowFrame(parent),
+    m_setter(ConfigSetter::instance())
 {
     QDesktopWidget dw;
-    int ww = dw.geometry().width() * 0.67 < MAINWIDGET_MINIMUN_WIDTH ? MAINWIDGET_MINIMUN_WIDTH : dw.geometry().width() * 0.67;
-    int wh = dw.geometry().height() * 0.62 < MAINWIDGET_MINIMUN_HEIGHT ? MAINWIDGET_MINIMUN_HEIGHT : dw.geometry().height() * 0.62;
+    const int defaultW = dw.geometry().width() * 0.67 < MAINWIDGET_MINIMUN_WIDTH
+            ? MAINWIDGET_MINIMUN_WIDTH : dw.geometry().width() * 0.67;
+    const int defaultH = dw.geometry().height() * 0.62 < MAINWIDGET_MINIMUN_HEIGHT
+            ? MAINWIDGET_MINIMUN_HEIGHT : dw.geometry().height() * 0.62;
+    const int ww = m_setter->value(SETTINGS_GROUP, SETTINGS_WINSIZE_W_KEY,
+                                   QVariant(defaultW)).toInt();
+    const int wh = m_setter->value(SETTINGS_GROUP, SETTINGS_WINSIZE_H_KEY,
+                                   QVariant(defaultH)).toInt();
     resize(ww, wh);
     setMinimumSize(MAINWIDGET_MINIMUN_WIDTH, MAINWIDGET_MINIMUN_HEIGHT);
     move((dw.geometry().width() - ww) / 2, (dw.geometry().height() - wh) / 4);
 
-    mainWidget = new MainWidget(manager, this);
-    addContenWidget(mainWidget);
 
+    m_mainWidget = new MainWidget(manager, this);
+    addContenWidget(m_mainWidget);
+}
+
+void MainWindow::resizeEvent(QResizeEvent *e)
+{
+    if (! isMaximized() && m_mainWidget->isVisible()) {
+        m_setter->setValue(SETTINGS_GROUP, SETTINGS_WINSIZE_W_KEY,
+                           QVariant(m_mainWidget->width()));
+        m_setter->setValue(SETTINGS_GROUP, SETTINGS_WINSIZE_H_KEY,
+                           QVariant(m_mainWidget->height()));
+    }
+    DWindowFrame::resizeEvent(e);
 }
