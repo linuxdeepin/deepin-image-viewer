77a81762dccd069b4b57d61bcf0254d91238e272
杨万青 <yangwanqing@deepin.com>
2016-07-15 09:50:20 +0800

    Fix: scrollbar cover by top and bottom panel
    Change-Id: Iccc060fb6ec2b34a62186e78d8da54815c9bdc11

--- widgets/scrollbar.cpp
+++ widgets/scrollbar.cpp
@@ -1,40 +1,9 @@
 #include "scrollbar.h"
-#include <QPropertyAnimation>
+#include "utils/baseutils.h"
 
 ScrollBar::ScrollBar(QWidget *parent)
-    : QScrollBar(parent)
+    : DScrollBar(parent)
 {
-    connect(this, &ScrollBar::valueChanged, this, &ScrollBar::onValueChange);
-    m_animation = new QPropertyAnimation(this, "value");
-    m_animation->setDuration(600);
-    m_animation->setEasingCurve(QEasingCurve::OutCubic);
+    setStyleSheet(utils::base::getFileContent(
+                      ":/qss/resources/qss/ScrollBar.qss"));
 }
-
-void ScrollBar::wheelEvent(QWheelEvent *event)
-{
-    // Fix me: the maximum() will change during wheel, i don't know why
-    if (m_animation->state() == QPropertyAnimation::Running) {
-        setValue(m_animation->endValue().toInt());
-        m_animation->stop();
-        m_setFromOutside = true;
-    }
-    else {
-        m_setFromOutside = false;
-    }
-    m_oldValue = this->value();
-    QScrollBar::wheelEvent(event);
-}
-
-void ScrollBar::onValueChange(int value)
-{
-    if (m_oldValue != 0) {
-        int cv = m_animation->currentValue().toInt();
-        // If value set from outside before, it should not use the cv for start value
-        m_animation->setStartValue(m_setFromOutside ? cv : m_oldValue);
-        m_animation->setEndValue(value);
-        m_oldValue = 0;
-        m_animation->start();
-    }
-}
-
-
--- widgets/scrollbar.h
+++ widgets/scrollbar.h
@@ -1,26 +1,13 @@
 #ifndef SCROLLBAR_H
 #define SCROLLBAR_H
 
-#include <QScrollBar>
+#include <dscrollbar.h>
 
-class QPropertyAnimation;
-class ScrollBar : public QScrollBar
+class ScrollBar : public Dtk::Widget::DScrollBar
 {
     Q_OBJECT
-    Q_PROPERTY(int value READ value WRITE setValue)
 public:
     explicit ScrollBar(QWidget *parent = 0);
-
-protected:
-    void wheelEvent(QWheelEvent *event) override;
-
-private:
-    void onValueChange(int value);
-
-private:
-    QPropertyAnimation *m_animation;
-    int m_oldValue;
-    bool m_setFromOutside;
 };
 
 #endif // SCROLLBAR_H
