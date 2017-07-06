a4fc10d67d8acdcb61b6f55b2cc8df11bd563a97
pangpangpang3 <penghui@linuxdeepin.com>
2017-07-06 11:16:45 +0800

    Fix:changed config's dir, need to delete database.
    Change-Id: I1ac4ecbd3e8606b1642074306ae8641ba245b510

--- viewer/controller/configsetter.cpp
+++ viewer/controller/configsetter.cpp
@@ -1,8 +1,25 @@
 #include "configsetter.h"
 #include <QDebug>
 
+#include <QDir>
+#include <QFileInfo>
+#include <QProcess>
+
+const QString CONFIG_PATH =   QDir::homePath() +
+        "/.config/deepin/deepin-image-viewer/config.conf";
+const QString DB_PATH = QDir::homePath() +
+        "/.local/share/deepin/deepin-image-viewer/deepinimageviewer.db";
+
 ConfigSetter::ConfigSetter(QObject *parent) : QObject(parent)
 {
+    if (!QFileInfo(CONFIG_PATH).exists()) {
+        QProcess* updateConfigProc = new QProcess(this);
+        updateConfigProc->startDetached(QString("rm %1").arg(DB_PATH));
+
+        connect(updateConfigProc, SIGNAL(finished(int)),
+                        updateConfigProc, SLOT(deleteLater()));
+    }
+
     m_settings = new  QSettings("deepin","/deepin-image-viewer/config", this);
     qDebug() << "Setting file:" << m_settings->fileName();
 }
