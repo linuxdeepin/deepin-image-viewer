1cb77f0f619292df399771f5b8a5681df6156a47
heyi <heyi@uniontech.com>
2020-03-30 17:10:17 +0800

    Title:fix Description:18448,18452,Start crash and left and right key operation crash
    RootCause:Null
    
    Solution:Rewrite load display logic

--- viewer/frame/mainwidget.cpp
+++ viewer/frame/mainwidget.cpp
@@ -379,7 +379,9 @@ void MainWidget::initConnection()
         }
     });
     connect(dApp->signalM, &SignalManager::sigShowFullScreen, this,
-            [ = ] { m_bottomToolbar->move((width() - m_bottomToolbar->width()) / 2, height()); });
+    [ = ] {
+        m_bottomToolbar->move((width() - m_bottomToolbar->width()) / 2, height());
+    });
 }
 
 void MainWidget::initBottomToolbar()
