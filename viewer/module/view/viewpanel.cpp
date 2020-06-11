38ba472762ff8fc3b9232cd77fc274c1b99545fc
shuwenzhi <shuwenzhi@uniontech.com>
2020-06-11 15:01:06 +0800

    Title: fix Description: 优化图片信息ＵＩ显示 RootCause:Null
    Solution:

--- viewer/module/view/viewpanel.cpp
+++ viewer/module/view/viewpanel.cpp
@@ -1344,7 +1344,6 @@ void ViewPanel::LoadDirPathFirst(bool bLoadAll)
         QMimeType mt = db.mimeTypeForFile(info.filePath, QMimeDatabase::MatchContent);
         QMimeType mt1 = db.mimeTypeForFile(info.filePath, QMimeDatabase::MatchExtension);
         QString str = m_AllPath.at(nStartIndex).suffix();
-        nStartIndex++;
         // if (!m_nosupportformat.contains(str, Qt::CaseSensitive)) {
         if (mt.name().startsWith("image/") || mt.name().startsWith("video/x-mng") ||
                 mt1.name().startsWith("image/") || mt1.name().startsWith("video/x-mng")) {
@@ -1379,10 +1378,12 @@ void ViewPanel::LoadDirPathFirst(bool bLoadAll)
                     m_infosAll.append(info);
                 }
             }
+            nStartIndex++;
+            i++;
+        }else {
+            m_AllPath.removeAt(nStartIndex);
+            nCount--;
         }
-
-        // }
-        i++;
     }
     if (!bLoadAll) m_lastindex = m_firstindex + nimgcount - 1;
 }
@@ -1514,17 +1515,17 @@ void ViewPanel::onViewImage(const SignalManager::ViewInfo &vinfo)
             QString DirPath = vinfo.path.left(vinfo.path.lastIndexOf("/"));
             QDir _dirinit(DirPath);
             m_AllPath = _dirinit.entryInfoList(QDir::Files | QDir::Hidden | QDir::NoDotAndDotDot, QDir::LocaleAware);
-            //LMH0611判断是否存在不是图片的文件，并从m_AllPath删除不是图片的文件路径
-            QFileInfoList errorstrList;
-            for(auto str:m_AllPath){
-                QString strSuffix = str.suffix();
-                if (!utils::image::supportedImageFormats().contains("*." + strSuffix, Qt::CaseInsensitive)) {
-                    errorstrList.push_back(str);
-                }
-            }
-            for(auto str:errorstrList){
-                m_AllPath.removeOne(str);
-            }
+//            //LMH0611判断是否存在不是图片的文件，并从m_AllPath删除不是图片的文件路径
+//            QFileInfoList errorstrList;
+//            for(auto str:m_AllPath){
+//                QString strSuffix = str.suffix();
+//                if (!utils::image::supportedImageFormats().contains("*." + strSuffix, Qt::CaseInsensitive)) {
+//                    errorstrList.push_back(str);
+//                }
+//            }
+//            for(auto str:errorstrList){
+//                m_AllPath.removeOne(str);
+//            }
 
             m_current = 0;
             for (; m_current < m_AllPath.size(); m_current++) {
