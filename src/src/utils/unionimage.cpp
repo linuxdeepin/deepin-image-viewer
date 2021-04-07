357ac5d187c636c53297ba31ee3091a078ed0d3b
liuminghang <liuminghang@uniontech.com>
2021-04-07 10:46:15 +0800

    fix: 解决bug69724
    Description: 因为旋转你的真实格式没有读取正确，现在改用正确的方式读取
    
    Log: 与旋转有关
    Bug: https://pms.uniontech.com/zentao/bug-view-69724.html
    Change-Id: I0613df69d1efc722c2cde713c9789cae18726160

--- src/src/utils/unionimage.cpp
+++ src/src/utils/unionimage.cpp
@@ -742,100 +742,10 @@ UNIONIMAGESHARED_EXPORT bool loadStaticImageFromFile(const QString &path, QImage
 
 UNIONIMAGESHARED_EXPORT QString detectImageFormat(const QString &path)
 {
-    FREE_IMAGE_FORMAT f = FreeImage_GetFileType(path.toUtf8().data());
-    QString res = union_image_private.m_freeimage_formats.key(f);
-    if (res.isEmpty()) {
-        QFile file(path);
-        if (!file.open(QIODevice::ReadOnly)) {
-            return "";
-        }
-
-        //    const QByteArray data = file.read(1024);
-        const QByteArray data = file.read(64);
-
-        // Check bmp file.
-        if (data.startsWith("BM")) {
-            return "BMP";
-        }
-
-        // Check dds file.
-        if (data.startsWith("DDS")) {
-            return "DDS";
-        }
-
-        // Check gif file.
-        if (data.startsWith("GIF8")) {
-            return "GIF";
-        }
-
-        // Check Max OS icons file.
-        if (data.startsWith("icns")) {
-            return "ICNS";
-        }
-
-        // Check jpeg file.
-        if (data.startsWith("\xff\xd8")) {
-            return "JPG";
-        }
-
-        // Check mng file.
-        if (data.startsWith("\x8a\x4d\x4e\x47\x0d\x0a\x1a\x0a")) {
-            return "MNG";
-        }
-
-        // Check net pbm file (BitMap).
-        if (data.startsWith("P1") || data.startsWith("P4")) {
-            return "PBM";
-        }
-
-        // Check pgm file (GrayMap).
-        if (data.startsWith("P2") || data.startsWith("P5")) {
-            return "PGM";
-        }
-
-        // Check ppm file (PixMap).
-        if (data.startsWith("P3") || data.startsWith("P6")) {
-            return "PPM";
-        }
-
-        // Check png file.
-        if (data.startsWith("\x89PNG\x0d\x0a\x1a\x0a")) {
-            return "PNG";
-        }
-
-        // Check svg file.
-        if (data.indexOf("<svg") > -1) {
-            return "SVG";
-        }
-
-        // TODO(xushaohua): tga file is not supported yet.
-
-        // Check tiff file.
-        if (data.startsWith("MM\x00\x2a") || data.startsWith("II\x2a\x00")) {
-            // big-endian, little-endian.
-            return "TIFF";
-        }
-
-        // TODO(xushaohua): Support wbmp file.
-
-        // Check webp file.
-        if (data.startsWith("RIFFr\x00\x00\x00WEBPVP")) {
-            return "WEBP";
-        }
-
-        // Check xbm file.
-        if (data.indexOf("#define max_width ") > -1 &&
-                data.indexOf("#define max_height ") > -1) {
-            return "XBM";
-        }
-
-        // Check xpm file.
-        if (data.startsWith("/* XPM */")) {
-            return "XPM";
-        }
-        return "";
-    }
-    return res;
+    //用freeimage的格式来做判断
+    QMap<QString, QString> dataMap = getAllMetaData(path);
+    const QString suffix = dataMap.value("FileFormat");
+    return suffix.toUpper();
 }
 
 FREE_IMAGE_FORMAT detectImageFormat_f(const QString &path)
