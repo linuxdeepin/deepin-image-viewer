c3ae2c4f9db49d26a1693a9821c0e2f688f12b02
liuminghang <liuminghang@uniontech.com>
2021-05-27 11:39:35 +0800

    fix: 修复bug81496
    Description: 翻译没有空格，现在已经加上
    
    Log: 与翻译有关
    Bug: https://pms.uniontech.com/zentao/bug-view-81496.html
    Change-Id: I398d9a8d840d9dcd34b6b357bef5d2f9a8b8bddf

--- src/translations/deepin-image-viewer_zh_CN.ts
+++ src/translations/deepin-image-viewer_zh_CN.ts
@@ -1,4 +1,6 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="zh_CN" version="2.1">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
 <context>
     <name>Application</name>
     <message>
@@ -247,11 +249,11 @@
     <name>RenameDialog</name>
     <message>
         <source>Confirm</source>
-        <translation>确定</translation>
+        <translation>确 定</translation>
     </message>
     <message>
         <source>Cancel</source>
-        <translation>取消</translation>
+        <translation>取 消</translation>
     </message>
     <message>
         <source>Input a new name</source>
