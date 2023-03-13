ec9a464a0160ef6a7d0d58846eab672f3fefbf61
renbin <renbin@uniontech.com>
2023-03-13 18:23:33 +0800

    feat: Merge permission/watermark into master
    合并权限控制和水印需求到主线
    结合imageeditor添加授权控制及水印.
    更新Eventlog接口，添加广播授权操作.
    
    Log: 结合imageeditor添加授权控制及水印.
    Task: https://pms.uniontech.com/story-view-27785.html,
          https://pms.uniontech.com/story-view-30217.html
    Influence: 数据埋点 广播授权操作
    Change-Id: Id072e255fe9fa76c0117e2628f0e59d1b258e2d6

--- src/src/utils/eventlogutils.cpp
+++ src/src/utils/eventlogutils.cpp
@@ -1,12 +1,14 @@
-// SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
+// SPDX-FileCopyrightText: 2022 - 2024 UnionTech Software Technology Co., Ltd.
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #include "eventlogutils.h"
+
 #include <QLibrary>
 #include <QDir>
 #include <QLibraryInfo>
 #include <QJsonDocument>
+#include <QDebug>
 
 Eventlogutils *Eventlogutils::m_pInstance = nullptr;
 Eventlogutils *Eventlogutils::GetInstance()
@@ -17,7 +19,7 @@ Eventlogutils *Eventlogutils::GetInstance()
     return m_pInstance;
 }
 
-void Eventlogutils::writeLogs(QJsonObject &data)
+void Eventlogutils::writeLogs(const QJsonObject &data)
 {
     if (!writeEventLogFunc)
         return;
@@ -25,14 +27,50 @@ void Eventlogutils::writeLogs(QJsonObject &data)
     writeEventLogFunc(QJsonDocument(data).toJson(QJsonDocument::Compact).toStdString());
 }
 
+void Eventlogutils::sendLogs(const QJsonObject &data)
+{
+    if (!sendEventLogFunc)
+        return;
+
+    sendEventLogFunc(QJsonDocument(data).toJson(QJsonDocument::Compact).toStdString());
+}
+
+/**
+   @return 返回当前 sendLogs() 接口是否可用，不同 EventLog 版本支持的接口不同，
+        旧版本仅提供 writeLogs()
+ */
+bool Eventlogutils::sendLogsEnabled() const
+{
+    return  nullptr != sendEventLogFunc;
+}
+
+/**
+   @brief 转发外部发送的埋点数据 `data`，此接口仅用于1.2及之后版本的数据埋点
+ */
+void Eventlogutils::forwardLogData(const QJsonObject &data)
+{
+    qInfo() << qPrintable("EventLog:") << data;
+    sendLogs(data);
+}
+
 Eventlogutils::Eventlogutils()
 {
     QLibrary library("libdeepin-event-log.so");
     initFunc = reinterpret_cast<bool (*)(const std::string &, bool)>(library.resolve("Initialize"));
     writeEventLogFunc = reinterpret_cast<void (*)(const std::string &)>(library.resolve("WriteEventLog"));
+    sendEventLogFunc = reinterpret_cast<void (*)(const std::string &)>(library.resolve("SendEventLog"));
 
-    if (!initFunc)
+    if (!initFunc) {
+        qWarning() << qPrintable("Not resolve Eventlog api Initialize()");
         return;
+    }
+
+    if (!writeEventLogFunc) {
+        qWarning() << qPrintable("Not resolve Eventlog api WriteEventLog()");
+    }
+    if (!sendEventLogFunc) {
+        qWarning() << qPrintable("Not resolve Eventlog api SendEventLog()");
+    }
 
     initFunc("deepin-image-viewer", true);
 }
