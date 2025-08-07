cbabf5a4fb48c8b7abe5dbe8d0a11896aafbc2a2
JWWTSL <167396168+JWWTSL@users.noreply.github.com>
2025-08-07 19:02:15 +0800

    feat: Update translations for Lao locale (#259)
    log: Translation update

---
+++ src/translations/desktop/desktop_lo.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="lo">
+<context>
+    <name>desktop</name>
+    <message>
+        <location filename="Desktop Entry]Comment" line="0"/>
+        <source>Deepin Image Viewer</source>
+        <translation>ຕົວເບິ່ງຮູບພາບ Deepin</translation>
+    </message>
+    <message>
+        <location filename="Desktop Entry]GenericName" line="0"/>
+        <source>Image Viewer</source>
+        <translation>ຕົວເບິ່ງຮູບພາບ</translation>
+    </message>
+    <message>
+        <location filename="Desktop Entry]Keywords" line="0"/>
+        <source>Image;Viewer;Jpg;Jpeg;Png;</source>
+        <translation>ຮູບພາບ;ຕົວເບິ່ງ;Jpg;Jpeg;Png;</translation>
+    </message>
+</context>
+</TS>
